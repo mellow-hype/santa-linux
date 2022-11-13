2249feb886dccdee9f2ef4de2de67a3e5514fde0
mellow-hype <hyper@mellow>
2022-11-12 22:15:39 -0800

    working xpc with threads

--- src/rust_santa_daemon/src/santactl/main.rs
+++ src/rust_santa_daemon/src/santactl/main.rs
@@ -1,22 +1,28 @@
 // Local imports
-use libsanta::uxpc::{SantaXpcClient, SantaXpcCtlServer};
+use libsanta::uxpc::{SantaXpcClient, SantaXpcServer};
 use libsanta::{XPC_SOCKET_PATH, XPC_CLIENT_PATH};
-use fork::{Fork, daemon};
+use std::time::Duration;
+use std::thread;
 
 fn main() {
     let path = String::from(XPC_CLIENT_PATH);
-    let server = SantaXpcCtlServer::new(path, false);
+    let server = SantaXpcServer::new(path, false);
 
-    // create the server socket in a child process
-    if let Ok(Fork::Child) = daemon(false, true) {
-        loop {
-            if let Some(asdf) = server.recv() {
-                println!("santactl xpc server socket got: {asdf}")
-            }
-        }
+    // send the command msg in a thread
+    thread::spawn(move || {
+        // create the client socket and send the message
+        let mut client = SantaXpcClient::new(String::from(XPC_SOCKET_PATH));
+        client.send(b"status").unwrap();
+
+        // sleep to give the listener a chance to recv the message
+        thread::sleep(Duration::from_millis(50));
+    });
+
+    // sleep to give the sender thread a chance to send the message
+    thread::sleep(Duration::from_millis(50));
+
+    // wait for the response
+    if let Some(asdf) = server.recv() {
+        println!("{asdf}");
     }
-
-    // create the client socket
-    let mut client = SantaXpcClient::new(String::from(XPC_SOCKET_PATH));
-    client.send(b"status").unwrap();
 }
